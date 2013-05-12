7048d98b5d1491d7ea0813b7fb6fd375eb0c4855
Gal Dolber <gal.dolber@gmail.com>
2013-05-12 19:55:52 -0300

    Update deps

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot :all
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [appengy "0.1.4"]
+                 [appengy "0.1.5"]
                  [org.clojure/tools.cli "0.2.2"]
                  [ring/ring-devel "1.2.0-beta2"]])
