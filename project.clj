b8a7919eb5bc5963479440113a21c9ab1de4bb1c
Gal Dolber <gal.dolber@gmail.com>
2013-05-12 20:00:46 -0300

    Fix main

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot :all
+  :main appengy.AppengySimple
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [appengy "0.1.5"]
                  [org.clojure/tools.cli "0.2.2"]
