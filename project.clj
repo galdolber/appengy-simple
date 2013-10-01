8dcb7453a1199f582b28320b1f966e361864a31c
Gal Dolber <gal.dolber@gmail.com>
2013-10-01 19:59:37 -0300

    0.1.11

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :aot :all
   :main appengy.AppengySimple
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [appengy "0.1.10"]
+                 [appengy "0.1.11"]
                  [org.clojure/tools.cli "0.2.2"]
                  [ring/ring-devel "1.2.0-beta2"]])
