3d2cf49830bdd0480cb58fa7de75ffb9cebc9d7e
Gal Dolber <gal.dolber@gmail.com>
2014-06-18 00:06:06 +0200

    12

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject appengy-simple "0.1.11"
+(defproject appengy-simple "0.1.12"
   :description "Simple appengy server"
   :url "http://github.com/galdolber/appengy-simple"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot :all
   :main appengy.AppengySimple
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [appengy "0.1.11"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [appengy "0.1.12"]
                  [org.clojure/tools.cli "0.2.2"]
                  [ring/ring-devel "1.2.0-beta2"]])
